d1f42d1aaf60713ed2a141b3bd8f9037a89161f3
Fernando <dacrazed@gmail.com>
2021-10-29 16:43:28 +1100

    Use SFML for rendering on the XWindow

--- main.cpp
+++ main.cpp
@@ -8,16 +8,20 @@
 #include <sstream>
 #include <vector>
 #include <map>
+#include <SFML/Graphics.hpp>
 //#include <windows.h>
 #include <stdlib.h>
 #include <chrono>
-#include <future>
 #include <X11/Xlib.h>
 #include <unistd.h>
 #include "const.h"
 #include "renderer/objects/Vector2.h"
 #include "renderer/objects/Vector3.h"
 #include "renderer/raymarch.h"
+#include <future>
+#include <unordered_map>
+#include <X11/Xlib.h>
+
 
 #define ENABLE_VIRTUAL_TERMINAL_PROCESSING 0x0004
 #define DISABLE_NEWLINE_AUTO_RETURN 0x0008
@@ -87,11 +91,60 @@ std::string generateFrame(int h, int w)
 	return frames;
 }
 
-std::string *generateFrame2(int h, int w)
+void generateSFMLFrame(uint32_t* array, int h, int w) {
+	for (int x = 0; x < h; x++) // we render every second pixel strip to compensate for windows being EXTREMELY idiotic and having line spacing
+	{
+		for (int y = 0; y < w; y++)
+		{
+			vectors::Vector3 coler = mainImage(vectors::Vector2d(y + 1, x + 1));
+			array[x*w+y]= int( (uint8_t)floor(coler.x * 255) << 24 |
+				(uint8_t)floor(coler.y * 255) << 16 |
+				(uint8_t)floor(coler.z * 255) << 8 |
+				255);
+		}
+	}
+}
+
+std::unordered_map<uint32_t, std::vector<sf::Vector2i>> generateColourLayers(
+	uint32_t* array, int h, int w
+) {
+	std::unordered_map<uint32_t, std::vector<sf::Vector2i>>  hatred = std::unordered_map<uint32_t, std::vector<sf::Vector2i>>();
+	for (int x = 0; x < h; x++)
+	{
+		for (int y = 0; y < w; y++)
+		{
+			if (hatred.find(array[x * w + y]) == hatred.end())
+			{
+				hatred.insert({
+					array[w*w+y],
+					std::vector<sf::Vector2i>()
+				});
+				hatred[array[x * w + y]].push_back({short(x), short(y)});
+			}
+			else
+			{
+				hatred[array[x * w + y]].push_back({short(x), short(y)});
+			}
+		}
+	}
+	return hatred;
+}
+
+sf::Sprite renderSFMLFrame(std::unordered_map<uint32_t, std::vector<sf::Vector2i>> colorLayer, int w, int h, sf::Image& img, sf::Texture& texture) {
+	img.create(w, h);
+	for (auto it = colorLayer.begin(); it != colorLayer.end(); ++it) {
+		sf::Color colour(it->first);
+		for (const sf::Vector2i& pixel: it->second) {
+			img.setPixel(pixel.x, pixel.y, colour);
+		}
+	}
+
+	texture.loadFromImage(img);
+	return sf::Sprite(texture);
+}
+
+void generateFrame2(std::string* frame, int h, int w)
 {
-	// GC anus = create_gc(d, wind, 0);
-	//Pixmap frames = XCreatePixmap(d, wind, h, w, 8);
-	std::string *amogus = new std::string[h * w];
 	for (int x = 0; x < h; x++) // we render every second pixel strip to compensate for windows being EXTREMELY idiotic and having line spacing
 	{
 		for (int y = 0; y < w; y++)
@@ -102,8 +155,7 @@ std::string *generateFrame2(int h, int w)
 #endif
 			//COLORREF color = RGB(coler.x * 255, coler.y * 255, coler.z * 255);
 #ifndef DEBUG
-			std::string colerstr = "RGBi:" + std::to_string(coler.x) + "/" + std::to_string(coler.y) + "/" + std::to_string(coler.z);
-			amogus[x * w + y] = colerstr;
+			frame[x * w + y] = "RGBi:" + std::to_string(coler.x) + "/" + std::to_string(coler.y) + "/" + std::to_string(coler.z);
 
 #else
 			std::cout << std::endl;
@@ -111,8 +163,6 @@ std::string *generateFrame2(int h, int w)
 #endif
 		}
 	}
-	//return frames;
-	return amogus;
 }
 
 std::map<std::string, std::vector<XPoint>> generateColourLayers(std::string *colourData, int h, int w)
@@ -173,13 +223,18 @@ int windowRender()
 {
 	// Open a display.
 	Display *d = XOpenDisplay(0);
+	std::cout << "Display create" << std::endl;
 
 	if (d)
 	{
+
 		// Create the window
-		Window wind = XCreateWindow(d, DefaultRootWindow(d), 0, 0, RESOLUTION.x,
-									RESOLUTION.y, 0, CopyFromParent, CopyFromParent,
-									CopyFromParent, 0, 0);
+		Window wind = XCreateWindow(d, DefaultRootWindow(d), 
+			0, 0, RESOLUTION.x, RESOLUTION.y, 0,
+			DefaultDepth(d, DefaultScreen(d)), 
+			InputOutput, 
+			DefaultVisual(d, DefaultScreen(d)), 
+			CopyFromParent, 0);
 
 		// Show the window
 		XMapWindow(d, wind);
@@ -202,25 +257,35 @@ int windowRender()
 		w = RESOLUTION.x;
 		h = RESOLUTION.y;
 
+		sf::RenderWindow SFMLView(wind);
+
+		std::string* _drawFrame = new std::string[w*h];
+		uint32_t* sfmlFrame = new uint32_t[w*h];
+		sf::Image renderImage;
+		sf::Texture renderTexture;
+		
 		// Sleep long enough to see the window.
 		while (true)
 		{
-			//generateFrame2(d, w, gc, cm, RESOLUTION.x, RESOLUTION.y);
 			start = std::chrono::system_clock::now();
 			frame(fps);
-			std::future<std::string *> fut = std::async(generateFrame2, h, w);
-			fut.wait();
-			std::string *mogus = fut.get();
+			generateSFMLFrame(sfmlFrame, h, w);
+			sf::Sprite renderSprite = renderSFMLFrame(
+				generateColourLayers(sfmlFrame, h, w),
+				w, h, renderImage, renderTexture
+			);
+
 			auto end = std::chrono::system_clock::now();
 			std::chrono::duration<double> elapsed_seconds = end - start;
 			fps = (int)floor(1 / elapsed_seconds.count());
-			renderFrame(d, wind, gc, cm, h, w, generateColourLayers(mogus, h, w));
-			XColor colero;
-			XAllocNamedColor(d, cm, "white", &colero, &colero);
-			XSetForeground(d, gc, colero.pixel);
-			std::string fpsmes = "FPS: " + std::to_string(fps);
-			char *ch = &fpsmes[0];
-			XDrawString(d, wind, gc, 0, 0, ch, strlen(ch));
+
+			while (XPending(d)) {
+				XEvent event;
+				XNextEvent(d, &event);
+			}
+			SFMLView.clear();
+			SFMLView.draw(renderSprite);
+			SFMLView.display();
 		}
 	}
 	return 0;
@@ -228,6 +293,7 @@ int windowRender()
 
 int main()
 {
+	std::cout << "Hello world!" << std::endl;
 #ifdef NOTERM
 	return windowRender();
 #endif
